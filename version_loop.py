970821cf3eab43ccdde788a6646712406368569b
Dianliang <dianliang233@gmail.com>
2020-07-20 03:19:57 +0800

    Update version_loop.py

--- version_loop.py
+++ version_loop.py
@@ -2,17 +2,18 @@ import requests
 import json
 import time
 import os,sys
+import yaml
 
-def versionLoop(mojira):
+def versionLoop(mojira, rate):
     try:
         os.remove('version_loop/version_cache.txt')
     except Exception:
         pass
-    manifest_raw = requests.get('http://launchermeta.mojang.com/mc/game/version_manifest.json')
+    manifest_raw = requests.get('http://launchermeta.mojang.com/mc/game/version_manifest.json',timeout=rate)
     file = json.loads(version_manifest.text)
-    server.tell(info, "最新快照:" + file['latest']['snapshot'] + "，每15秒侦测一次。")
+    server.tell(info, "最新快照:" + file['latest']['snapshot'] + "，每" + rate + "秒侦测一次。")
     if mojira == True:
-        mojira_raw = requests.get('https://bugs.mojang.com/rest/api/2/project/10400')
+        mojira_raw = requests.get('https://bugs.mojang.com/rest/api/2/project/10400',timeout=rate)
         mojira_json = json.loads(mojira_raw.text)
         mojira_version = []
         for Version in mojira_json[:]:
@@ -37,7 +38,7 @@ def versionLoop(mojira):
         try:
             cache_read = open('version_loop/version_cache.txt',mode='r',encoding='utf-8')
             cache_readed = open('version_loop/version_cache.txt',mode='r',encoding='utf-8').read()
-            manifest_raw = requests.get('http://launchermeta.mojang.com/mc/game/version_manifest.json',timeout=20)
+            manifest_raw = requests.get('http://launchermeta.mojang.com/mc/game/version_manifest.json',timeout=rate)
             manifest = json.loads(version_manifest.text)
             if manifest['latest']['snapshot'] == cache_readed[0]:
                 print("没有侦测到新快照。")
@@ -54,7 +55,7 @@ def versionLoop(mojira):
                 cache_read.close()
                 os.remove('version_loop/version_cache.txt')
             if mojira == True
-                mojira_raw = requests.get('https://bugs.mojang.com/rest/api/2/project/10400')
+                mojira_raw = requests.get('https://bugs.mojang.com/rest/api/2/project/10400',timeout=rate)
                 mojira_json = json.loads(mojira_raw.text)
                 mojira_version = []
                 for Version in mojira_json[:]:
@@ -71,13 +72,44 @@ def versionLoop(mojira):
                     server.tell(info, "在Mojira上的新版本:" + file['latest']['snapshot'] + "。")
                     cache_read.close()
             cache_write.write(launched)
-            time.sleep(15)
+            time.sleep(rate)
             except requests.exceptions.RequestException:
                 print("发生错误：无法获取信息。")
-                time.sleep(15)
+                time.sleep(rate)
                 error_times += 1
                 continue
             except requests.exceptions.Timeout:
                 print("发生错误：超时。")
                 error_times += 1
                 continue
+
+def on_server_startup ():
+    if os.path.exists("./config/version-loop.yaml"):
+        yaml_raw = open("./config/version-loop.yaml", 'r', encoding="utf-8").read()
+        yaml_data = yaml.load(yaml_raw)
+    else: 
+        if os.path.exists("./config/version-loop.yml"):
+            yaml_raw = open("./config/version-loop.yml", 'r', encoding="utf-8").read()
+            yaml_data = yaml.load(yaml_raw)
+        else:
+            yaml_text = """
+###########################################
+#      A TEAHOUSE STUDIOS PRODUCTION      #
+#       Version_loop from _LittleK_       #
+#           CONFIGURATION FILE            #
+###########################################
+#      Copyright 2020 Teahouse Studios    #
+#        (No licence avalabile yet)       #
+###########################################
+
+# Toggle this if you don't want Mojira detection
+mojira: true
+
+# This is the time that how long the script should wait (second) between queries
+rate: 15
+
+"""
+            yaml_raw = open("./config/version-loop.yml", 'a', encoding="utf-8").write(yaml_text)
+    yaml_mojira = yaml_data["mojira"]
+    yaml_rate = yaml_data["rate"]
+    versionLoop(yaml_mojira, yaml_rate)
