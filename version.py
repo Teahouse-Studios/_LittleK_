275338d2fa698e724d17feeef9d76ae8ba1853bf
Dianliang <dianliang233@gmail.com>
2020-07-19 22:18:06 +0800

    Update version.py
    Add Java Mojira
    Add Minecraft Earth
    Add help
    Improve description of `!!&version`
    Fix crash
    Fix an issue that Dungeons version won't display when using `!!&version`

--- version.py
+++ version.py
@@ -10,51 +10,76 @@ def javaVer():
     try:
         manifest_raw = requests.get('http://launchermeta.mojang.com/mc/game/version_manifest.json')
         manifest = json.loads(version_manifest.text)
-        return("最新版：" + manifest['latest']['release'] + "，最新快照：" + manifest['latest']['snapshot'] + '\n')
+        mojira_raw = requests.get('https://bugs.mojang.com/rest/api/2/project/10400')
+        mojira = json.loads(mojira_raw.text)
+        mojira_version = []
+        for Version in mojira[:]:
+            if Version['archived'] == False:
+                mojira_version.append(Version['name'])
+            else:
+                pass
+        pipe = '| '
+        mojira_version_name = pipe.join(mojira_version)
+        if manifest['latest']['release'] == mojira_version_name or manifest['latest']['snapshot'] == mojira_version_name:
+            return "最新版：" + manifest['latest']['release'] + "，最新快照：" + manifest['latest']['snapshot'] + '\n'
+        else:
+            return "最新版：" + manifest['latest']['release'] + "，最新快照：" + manifest['latest']['snapshot'] + '\nMojira上的最新版本：'+ mojira_version_name + '\n'
     except Exception:
-        return("发生错误：土豆熟了。\n")
+        return "发生错误：土豆熟了。\n"
 
 # Bedrock
 def bedrockVer():
-    Mojiraurl = 'https://bugs.mojang.com/rest/api/2/project/10200/versions'
-    getJSON = requests.get(Mojiraurl)
-    loadJSON = json.loads(getJSON.text)
-    Beta = []
-    Release = []
-    for Version in loadJSON[:]:
+    manifest_raw = requests.get('https://bugs.mojang.com/rest/api/2/project/10200/versions')
+    manifest = json.loads(manifest_raw.text)
+    beta = []
+    release = []
+    for Version in manifest[:]:
         if Version['archived'] == False:
             try:
                 findBeta = re.match(r'(.*)Beta$',Version['name'])
-                Beta.append(findBeta.group(1))
+                beta.append(findBeta.group(1))
             except Exception:
-                Release.append(Version['name'])
+                release.append(Version['name'])
         else:
             pass
     pipe = '| '
-    text1 = pipe.join(Beta)
-    text2 = pipe.join(Release)
-    return('Beta: '+text1+'\nRelease: '+text2+'\n')
+    beta_name = pipe.join(Beta)
+    release_name = pipe.join(Release)
+    return 'Beta: ' + beta_name + '\nRelease: ' + release_name` + '\n'
 
 # Dungeons
 def dungeonsVer():
-    Mojiraurl = 'https://bugs.mojang.com/rest/api/2/project/11901/versions'
-    getJSON = requests.get(Mojiraurl)
-    loadJSON = json.loads(getJSON.text)
-    Release = []
-    for Version in loadJSON[:]:
+    manifest_raw = requests.get('https://bugs.mojang.com/rest/api/2/project/10200/versions')
+    manifest = json.loads(manifest_raw.text)
+    release = []
+    for Version in manifest[:]:
         if Version['archived'] == False:
-            Release.append(Version['name'])
+            release.append(Version['name'])
         else:
             pass
     pipe = '| '
-    text1 = pipe.join(Release)
-    return('最新版：'+text1+'\n')
+    release_name = pipe.join(release)
+    return '最新版：' + release_name + '\n'
+
+# Earth
+def earthVer():
+    manifest_raw = requests.get('https://bugs.mojang.com/rest/api/2/project/11900')
+    manifest = json.loads(manifest_raw.text)
+    release = []
+    for Version in manifest[:]:
+        if Version['archived'] == False:
+            release.append(Version['name'])
+        else:
+            pass
+    pipe = '| '
+    release_name = pipe.join(release)
+    return '最新版：' + release_name + '\n'
 
 # Final output
 def on_user_info(server, info):
     # All
     if info.content.startswith("!!&version") or info.content.startswith("!!&mcv"):
-        server.tell(info, "Java版：\n" + javaVer() + "基岩版：\n" + bedrockVer() + "Minecraft Dungeons：\n")
+        server.tell(info, "Java版：\n" + javaVer() + "基岩版（Mojira）：\n" + bedrockVer() + "Minecraft Dungeons（Mojira）：\n" + dungeonsVer() + "Minecraft Earth（Mojira）：\n" + earthVer())
     # Java
     if info.content.startswith("!!&version java") or info.content.startswith("!!&version je") or info.content.startswith("!!&mcv java") or info.content.startswith("!!&mcv je") or info.content.startswith("!!&mcjv")
         server.tell(info, javaVer())
@@ -64,3 +89,6 @@ def on_user_info(server, info):
     # Dungeons
     if info.content.startswith("!!&version dungeons") or info.content.startswith("!!&version mcd") or info.content.startswith("!!&mcv dungeons") or info.content.startswith("!!&mcv mcd") or info.content.startswith("!!&mcdv")
         server.tell(info, dungeonsVer())
+    # Earth
+    if info.content.startswith("!!&version earth") or info.content.startswith("!!&version mce") or info.content.startswith("!!&mcv earth") or info.content.startswith("!!&mcv mce") or info.content.startswith("!!&mcev")
+        server.tell(info, earthVer())
